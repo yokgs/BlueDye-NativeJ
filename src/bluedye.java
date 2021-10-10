703b4c14faac9011e5ab5dc9590ebe511241e312
Yazid Slila <70753201+yokgs@users.noreply.github.com>
2021-10-10 17:59:02 +0100

    Create bluedye.java

---
+++ src/bluedye.java
@@ -0,0 +1,12 @@
+/*
+ * BlueDye NativeJ v0.1.0
+ */
+public class bluedye {
+  private double RED,GREEN,BLUE,ALPHA;
+  bluedye(){
+    this.RED=0;
+    this.GREEN=0;
+    this.BLUE=0;
+    this.ALPHA=0;
+  }
+}
